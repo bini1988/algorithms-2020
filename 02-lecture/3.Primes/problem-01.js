fc6a89e2e99a13f97431da63e5ed66f414981117
Nikolai <rus.nick@mail.ru>
2020-08-08 19:57:51 +0300

    Add primes 01 solution

---
+++ 02-lecture/3.Primes/problem-01.js
@@ -0,0 +1,25 @@
+
+function isPrime(n) {
+  for (let i = 2; i < n; i++) {
+    if (n % i == 0) return false;
+  }
+  return true;
+}
+
+/**
+ * Количество простых чисел от 1 до N через перебор делителей
+ */
+function prime(n) {
+  let count = 0;
+
+  for (let i = 2; i <= n; i++) {
+    if (isPrime(i)) count++;
+  }
+  return count;
+}
+
+module.exports = function ([line1]) {
+  const N = BigInt(line1);
+
+  return prime(N);
+};
