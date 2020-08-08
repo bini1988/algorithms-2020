d68b6e198c280773ece828bb53b5d436bc6c7687
Nikolai <rus.nick@mail.ru>
2020-08-08 16:38:39 +0300

    Add GCD problem 01 & 02 solutions

---
+++ 02-lecture/1.GCD/problem-02.js
@@ -0,0 +1,28 @@
+
+/**
+ * Поиск НОД через остаток
+ */
+function gcd(a, b) {
+  while (a && b) {
+    if (a > b) {
+      a = a % b;
+    } else {
+      b = b % a;
+    }
+  }
+  return a || b;
+}
+
+/**
+ * Рекурсивный поиск НОД через остаток
+ */
+function gcd2(a, b) {
+  return b ? gcd2(b, a % b) : a;
+}
+
+module.exports = function ([line1, line2]) {
+  const A = BigInt(line1);
+  const B = BigInt(line2);
+
+  return gcd2(A, B);
+};
