063771c31ff070b7afb5e522d901da1c9135c0de
Nikolai Karastelev <n.karastelev@credinform.ru>
2020-08-17 11:43:49 +0300

    Add bitboard problem 4 solution

--- 03-lecture/4.Bitboard - Дальнобойщики/problem-01.js
+++ 03-lecture/4.Bitboard - Дальнобойщики/problem-01.js
@@ -37,40 +37,37 @@ function fen2bitboard(fen = "") {
   return pieces;
 }
 
-function move(mask, dir, steps = 1n) {
+function move(mask, dir) {
+  const bounds = 0xFFFF_FFFF_FFFF_FFFFn;
+  const mR = 0x7F7F_7F7F_7F7F_7F7Fn;
+  const mL = 0xFEFE_FEFE_FEFE_FEFEn;
+
   switch (dir) {
-    case "t": return mask << (8n * steps); // Вверх
-    case "b": return mask >> (8n * steps); // Вниз
-    case "r": return mask << (1n * steps); // Вправо
-    case "l": return mask >> (1n * steps); // Влево
-    case "tr": return mask << (9n * steps); // Вверх/Вправо
-    case "br": return mask >> (7n * steps); // Вниз/Вправо
-    case "tl": return mask << (7n * steps); // Вверх/Влево
-    case "bl": return mask >> (9n * steps); // Вниз/Влево
+    case "t": return (mask << 8n) & bounds; // Вверх
+    case "b": return (mask >> 8n) & bounds; // Вниз
+    case "r": return (mask & mR) << 1n; // Вправо
+    case "l": return (mask & mL) >> 1n; // Влево
+    case "tr": return ((mask & mR) << 9n) & bounds; // Вверх/Вправо
+    case "br": return ((mask & mR) >> 7n) & bounds; // Вниз/Вправо
+    case "tl": return ((mask & mL) << 7n) & bounds; // Вверх/Влево
+    case "bl": return ((mask & mL) >> 9n) & bounds; // Вниз/Влево
     default: return mask;
   }
 }
 
 function movesOf(piece, board, whitesBoard, dirs = []) {
   let out = 0x0000_0000_0000_0000n;
-  let bounds = 0xFFFF_FFFF_FFFF_FFFFn;
 
   for (let dir of dirs) {
-    for (let i = 1n, mask = 0n; mask == 0n; i++) {
-      mask = move(piece, dir, i);
+    for (let mask = piece; mask; mask &= board) {
+      mask = move(mask, dir);
       out |= mask;
-      mask &= (board | 0xFF818181818181FFn);
-      mask &= bounds;
     }
   }
   // Нельзя занимать уже занятые позиции белых фигур
   out &= out ^ whitesBoard;
 
   return out;
-}
-
-function byOr(out, piece) {
-  return out | piece;
 }
 
 function compute(fen) {
@@ -81,27 +78,30 @@ function compute(fen) {
     whiteQueen,    // белый ферзь
   ] = pieces;
 
-  let board = pieces.reduce(byOr, 0n);
-  let whitesBoard = pieces.slice(0, 6).reduce(byOr, 0n);
+  let bounds = 0xFFFF_FFFF_FFFF_FFFFn;
+
+  // инвертированный bitboard: 1 - пустая клетка, 0 - клетка с фигурой
+  let inverseBoard = pieces
+    .reduce((out, piece) => out & (bounds - piece), bounds);
+
+  // bitboard с белыми фигурами: 0 - пустая клетка, 1 - клетка с фигурой
+  let whitesBoard = pieces.slice(0, 6)
+    .reduce((out, piece) => out | piece, 0n);
 
   // белая ладья
   let rookDirs = ["t", "r", "b", "l"];
-  let rook = movesOf(whiteRook, board, whitesBoard, rookDirs);
+  let rook = movesOf(whiteRook, inverseBoard, whitesBoard, rookDirs);
 
   // белый слон
   let bishopDirs = ["tr", "br", "tl", "bl"];
-  let bishop = movesOf(whiteBishop, board, whitesBoard, bishopDirs);
+  let bishop = movesOf(whiteBishop, inverseBoard, whitesBoard, bishopDirs);
 
   // белый ферзь
   let queenDirs = ["t", "r", "b", "l", "tr", "br", "tl", "bl"];
-  let queen = movesOf(whiteQueen, board, whitesBoard, queenDirs);
+  let queen = movesOf(whiteQueen, inverseBoard, whitesBoard, queenDirs);
 
   return [rook, bishop, queen];
 }
-
-compute("5k2/8/4Q3/8/5B2/2R5/8/3K4");
-// compute("5k2/1Pr5/4Q1n1/8/5B2/1R3p2/7q/1bK5");
-// compute("4k2r/3q4/8/6b1/8/P7/2P2K2/2BQ1n1R");
 
 module.exports = function ([line]) {
   return compute(line);
