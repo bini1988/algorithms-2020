43da11723ad6d34bf840adf2a7108deb9e6bcc22
Nikolai <rus.nick@mail.ru>
2020-08-30 19:17:57 +0300

    Coderef CountionSort problem

--- 06-lecture/1.Sort/CountingSort.js
+++ 06-lecture/1.Sort/CountingSort.js
@@ -8,14 +8,18 @@ function sort(arr) {
   const out = new Array(N);
   const tmp = [];
 
-  for (let i = 0, k = arr[i]; i < N; k = arr[++i]) {
+  for (let i = 0; i < N; i++) {
+    const k = arr[i];
+
     tmp[k] = tmp[k] || 0;
     tmp[k]++;
   }
   for (let i = 1; i < tmp.length; i++) {
     tmp[i] = (tmp[i - 1] || 0) + (tmp[i] || 0);
   }
-  for (let i = N - 1, k = arr[i]; i >= 0; k = arr[--i]) {
+  for (let i = N - 1; i >= 0; i--) {
+    const k = arr[i];
+
     out[--tmp[k]] = k;
   }
   return out;
