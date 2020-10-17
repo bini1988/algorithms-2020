296b209c7bc92fa8b1ba11d48ddcd94bf2c9fb00
Nikolai <rus.nick@mail.ru>
2020-10-17 12:58:19 +0300

    Impl floyd warshall alg

--- 15-lecture/utils.js
+++ 15-lecture/utils.js
@@ -19,10 +19,8 @@ function list(G) {
   const out = [];
 
   for(let u = 0; u < G.length; u++) {
-    for (let v of G[u]) {
-      let w = undefined;
-
-      [v, w] = Array.isArray(v) ? v : [v];
+    for (let v_w of G[u]) {
+      let [v, w] = Array.isArray(v_w) ? v_w : [v_w];
 
       if (v > u) {
         out.push([u, v, w]);
@@ -33,4 +31,25 @@ function list(G) {
   return out;
 }
 
-module.exports = { lettering, list };
+/**
+ * Возвращает матрицу смежности графа
+ * @param {Array<Array<number|[number, number]>>} G Граф заданный вектором смежности
+ * @returns {Array<Array<number>>}
+ */
+function matrix(G) {
+  const N = G.length;
+  const M = [];
+
+  for(let u = 0; u < N; u++) {
+    M[u] = Array.from({ length: N }).fill(Infinity);
+
+    for (let v_w of G[u]) {
+      let [v, w = 1] = Array.isArray(v_w) ? v_w : [v_w];
+
+      M[u][v] = w;
+    }
+  }
+  return M;
+}
+
+module.exports = { lettering, list, matrix };
