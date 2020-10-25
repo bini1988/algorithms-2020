4d884dee7559f7127e2e451f138f5a57b93c2920
Nikolai <rus.nick@mail.ru>
2020-10-25 19:59:20 +0300

    Add boyer moore alg impl

--- 21-lecture/utils.js
+++ 21-lecture/utils.js
@@ -14,5 +14,55 @@ function equal(a = "", b = "") {
   return true;
 }
 
+/**
+ * Наивный алгоритм поиска подстроки в строке
+ * @param {string} text Исходная строка
+ * @param {string} pattern Искомая подстрока
+ * @returns {number}
+ */
+function substr(text, pattern) {
+  let out = [];
+  let t = 0;
+  let p = 0;
 
-module.exports = { equal };
+  for (t = 0; t < text.length - pattern.length + 1; t++) {
+    for (p = 0; p < pattern.length; p++) {
+      if (text[t + p] !== pattern[p]) break;
+    }
+    if (p == pattern.length) {
+      out.push(t);
+      t += p;
+    }
+  }
+  return out;
+}
+
+/**
+ * Вычисление Z-функции
+ * @description Нахождение максимальной длины последовательности от i-го символа совпадающей с последовательностью начинающейся от начала исходной строки
+ * @param {string} src Исходная строка
+ * @returns {number[]} Массив максимальных длин совпадающих последовательностей
+ */
+function zFun(src) {
+  let Z = Array.from(src).fill(0);
+  let N = src.length;
+  let l = 0;
+  let r = 0;
+
+  Z[0] = N;
+
+  for (let i = 1; i < N; i++) {
+    if (i <= r) {
+      Z[i] = Math.min(Z[i - l], r - i + 1);
+    }
+    while ((Z[i] + i < N) && src[Z[i]] == src[Z[i] + i]) {
+      Z[i] += 1;
+    }
+    if (i + Z[i] - 1 > r) {
+      [l, r] = [i, i + Z[i] - 1]
+    }
+  }
+  return Z;
+}
+
+module.exports = { equal, substr, zFun };
--- 21-lecture/utils.test.js
+++ 21-lecture/utils.test.js
@@ -1,11 +1,25 @@
 const assert = require("assert");
-const { equal } = require("./utils");
+const { equal, substr, zFun } = require("./utils");
 
 describe("Функции над строками", function () {
   it("Равенство строк", function () {
-    assert.strictEqual(equal('молоко', 'молоко'), true);
+    assert.strictEqual(equal("молоко", "молоко"), true);
   });
   it("Строки не равны", function () {
-    assert.strictEqual(equal('молоко', 'колокол'), false);
+    assert.strictEqual(equal("молоко", "колокол"), false);
+  });
+  it("Индекс подстроки в строке", function () {
+    const src = "abacabadabacabafabacabadabacabadabacabafaba";
+
+    assert.deepStrictEqual(substr("SOMESTRING", "STRING"), [4]);
+    assert.deepStrictEqual(substr(src, "aba"), [0, 4, 8, 12, 16, 20, 24, 28, 32, 36, 40]);
+    assert.deepStrictEqual(substr(src, "ca"), [3, 11, 19, 27, 35]);
+    assert.deepStrictEqual(substr(src, "dabf"), []);
+  });
+  it("Z функция", function () {
+    assert.deepStrictEqual(zFun("abcabcabca"), [10, 0, 0, 7, 0, 0, 4, 0, 0, 1]);
+    assert.deepStrictEqual(zFun("abcabcdabcabcabc"), [16, 0, 0, 3, 0, 0, 0, 6, 0, 0, 6, 0, 0, 3, 0, 0]);
+    assert.deepStrictEqual(zFun("abcabcabcabcdabc"), [16, 0, 0, 9, 0, 0, 6, 0, 0, 3, 0, 0, 0, 3, 0, 0]);
+    assert.deepStrictEqual(zFun("abracadabra"), [11, 0, 0, 1, 0, 1, 0, 4, 0, 0, 1]);
   });
 });
