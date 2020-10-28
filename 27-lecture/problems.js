0c9b042cb14d535ac30d09d4fa66fc642f9fe419
Nikolai Karastelev <n.karastelev@credinform.ru>
2020-10-28 18:40:31 +0300

    Add 27 lecture problems

--- 27-lecture/problems.js
+++ 27-lecture/problems.js
@@ -40,4 +40,92 @@ function jumper_min(N, price = []) {
   return C[N];
 }
 
-module.exports = { jumper, jumper_min };
+/**
+ * Шахматный король на поле MxN находится в ячейке [1,1], сколько способов добраться до клетки [N,M] шагая только вправо или вниз
+ * @param {number} N
+ * @param {number} M
+ */
+function king(N, M) {
+  // K[i][j] количество способов достичь клетки [i,j]
+  const K = [];
+
+  for (let i = 0; i < N + 1; i++) {
+    K[i] = K[i] || [0];
+  }
+  for (let j = 0; j < M + 1; j++) {
+    K[0][j] = 0;
+  }
+  for (let i = 1; i < N + 1; i++) {
+    for (let j = 1; j < M + 1; j++) {
+      if (i === 1 && j === 1) {
+        K[i][j] = 1;
+      } else {
+        K[i][j] = K[i - 1][j] + K[i][j - 1];
+      }
+    }
+  }
+  return K[N][M];
+}
+
+/**
+ * Длина наибольшей общей подпоследовательности между двумя массивами чисел A и B
+ * @description Массив C (подпоследовательность) содержащий элементы A в исходном порядке но возможно не все
+ * @param {number[]} A Массив длиной N
+ * @param {number[]} B Массив длиной M
+ */
+function subsequence(A = [], B = []) {
+  // F[i][j] - длина наибольшей возможной подпоследовательности частей A и B
+  // A[0:i] - часть A с первыми i элементами
+  // B[0:i] - часть B с первыми j элементами
+  //           -> 1 + F[i-1][j-1], если A[i] === B[i]
+  // F[i][j] = |
+  //           -> max(F[i][j-1], F[i-1][j]), если A[i] !== B[i]
+  // F[0][j] = 0; F[i][0] = 0;
+  const F = [];
+  const N = A.length;
+  const M = B.length;
+
+  for (let i = 0; i < N + 1; i++) {
+    F[i] = [];
+    for (let j = 0; j < M + 1; j++) {
+      F[i][j] = 0;
+    }
+  }
+
+  for (let i = 1; i < N + 1; i++) {
+    for (let j = 1; j < M + 1; j++) {
+      if (A[i-1] === B[j-1]) {
+        F[i][j] = 1 + F[i-1][j-1];
+      } else {
+        F[i][j] = Math.max(F[i][j-1], F[i-1][j]);
+      }
+    }
+  }
+  return F[N][M];
+}
+
+/**
+ * Длина наибольшая возрастающая подпоследовательность для массива чисел A
+ * @description Массив C (подпоследовательность) содержащий элементы A в исходном порядке но возможно не все
+ * @param {number[]} A Массив длиной N
+ */
+function g_subsequence(A = []) {
+  // F[i] - длина наибольшей возрастающей подпоследовательности для среза A[0:i], которорая заканчивается и сожержит элемент ai = A[i-1]
+  // F[i] = max(F[j]) + 1, если A[i] > A[j], j < i
+  // F[0] = 0;
+  const F = Array.from(A).fill(Infinity);
+  const N = A.length;
+
+  for (let i = 0; i < N; i++) {
+    F[i] = 1;
+
+    for (let j = 0; j < i; j++) {
+      if (A[i] > A[j]) {
+        F[i] = Math.max(F[i], F[j] + 1);
+      }
+    }
+  }
+  return Math.max(...F);
+}
+
+module.exports = { jumper, jumper_min, king, subsequence, g_subsequence };
